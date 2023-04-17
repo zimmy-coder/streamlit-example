867f4bc2b4a6d4afdd641e61832394995ec48038
zimmy-coder <130921779+zimmy-coder@users.noreply.github.com>
2023-04-17 20:27:08 +0530

    streamlit run app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,23 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+pip install streamlit
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+def largest_num(a, b, c):
+    if a>=b and a>=c:
+        return a
+    elif b>=a and b>=c:
+        return b
+    else:
+        return c
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+def main():
+    st.title("Find the Largest Number")
+    st.write("Enter three numbers below:")
+    a = st.number_input("Enter first number")
+    b = st.number_input("Enter second number")
+    c = st.number_input("Enter third number")
+    if st.button("Find Largest"):
+        result = largest_num(a, b, c)
+        st.write("The largest number is: ", result)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if __name__ == "__main__":
+    main()
